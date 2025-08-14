0263f791fd1bcadac6e532ec5b5b08f8c08eae8d
Yash Raj Dubey <94584095+Yashraj1805@users.noreply.github.com>
2025-08-15 00:09:21 +0530

    Update feedbackRoutes.js

--- routers/feedbackRoutes.js
+++ routers/feedbackRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { createFeedback, getFeedbacks } = require("../controllers/feedbackController");
-const auth = require("../middlewares/auth");
+
 
 const feedbackRouter = express.Router();
 
-feedbackRouter.post("/createFeedback",auth, createFeedback);
+feedbackRouter.post("/createFeedback", createFeedback);
 feedbackRouter.get("/getFeedbacks", getFeedbacks);
 
 module.exports = feedbackRouter;
