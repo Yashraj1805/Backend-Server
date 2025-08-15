f32748b35f27a0b42faff583c728606433891fc5
Yash Raj Dubey <94584095+Yashraj1805@users.noreply.github.com>
2025-08-15 15:29:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ dotenv.config();
 const app = express();
 connDB();
 
-app.use(cors())
+app.use(cors({
+    origin: "https://www.humotionai.com", // Exact frontend domain
+    credentials: true
+}))
 app.use(express.json());
 
 app.use('/api',router)
