67e3eb36f3083d50434b8ed78b07c540020add74
Yash Raj Dubey <94584095+Yashraj1805@users.noreply.github.com>
2025-08-15 19:42:37 +0530

    Update authRoutes.js

--- routers/authRoutes.js
+++ routers/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login, getProfile } = require("../controllers/authController");
+const { register, login, getProfile,updateProfile,deleteProfile } = require("../controllers/authController");
 const auth = require("../middlewares/auth");
 
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/getProfile", auth, getProfile);
-
+router.put('/updateprofile', auth, updateProfile);
+router.delete('/deleteprofile', auth, deleteProfile);
 
 module.exports = router;
